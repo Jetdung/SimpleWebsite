966adc49d96025e4d3cd0ee685435332ff68b77a
Jetdung <yzd315695355@hotmail.com>
2018-04-04 14:44:35 +0800

    Improved

--- src/main/java/com/yzd/classes/BaseDAO.java
+++ src/main/java/com/yzd/classes/BaseDAO.java
@@ -1,20 +1,37 @@
-﻿package com.yzd.classes;
+package com.yzd.classes;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class BaseDAO {
     Connection con=null;
-    PreparedStatement ps;
-    ResultSet rs;
+    PreparedStatement ps=null;
+    ResultSet rs=null;
     int count;
-    public static Connection getConn() throws Exception {                   //连接数据库
-        Class.forName("com.mysql.jdbc.Driver");
-        String url="jdbc:mysql://localhost:3306/system?useSSL=false";
-        String username="root";
-        String password="123456";
-        Connection conn= DriverManager.getConnection(url,username,password);
+    public static Connection getConn(){                             //连接数据库
+        Connection conn=null;
+        Properties p=new Properties();
+        try {
+            InputStream inputStream = BaseDAO.class.getResourceAsStream("/jdbc.properties");
+            p.load(inputStream);
+            Class.forName(p.getProperty("classname"));
+            String url=p.getProperty("url");
+            String username=p.getProperty("username");
+            String password=p.getProperty("password");
+            conn= DriverManager.getConnection(url,username,password);
+        }catch(IOException e){
+           e.printStackTrace();
+        } catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
         return conn;
     }
+
     public ResultSet exeQuery(String sql,Object[] values){                    //查询数据库
         try{
             con=getConn();
@@ -43,10 +60,11 @@ public class BaseDAO {
         }
         return count;
     }
+
     public void closeAll(){
         try {
-			rs.close();
-			ps.close();
+            rs.close();
+            ps.close();
             con.close();
         } catch (SQLException e) {
             e.printStackTrace();
